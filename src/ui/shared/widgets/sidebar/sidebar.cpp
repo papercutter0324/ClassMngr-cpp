0d6bb5f553213bb7db66d76dce65f49cb52d972f
papercutter0324 <wfeltmate@gmail.com>
2026-07-01 02:39:00 +0900

    Initial work to standardize node arrows in the sidebar

--- src/ui/shared/widgets/sidebar/sidebar.cpp
+++ src/ui/shared/widgets/sidebar/sidebar.cpp
@@ -9,6 +9,9 @@
 #include <QFontMetrics>
 #include <QHeaderView>
 #include <QMenu>
+#include <QPainter>
+#include <QPainterPath>
+#include <QPalette>
 #include <QProxyStyle>
 #include <QResizeEvent>
 #include <QStyleOption>
@@ -44,35 +47,8 @@ public:
         {
             if (option->state & QStyle::State_Children)
             {
-                QStyleOption arrowOption(*option);
-                const int indicatorSize =
-                    QProxyStyle::pixelMetric(
-                        QStyle::PM_IndicatorWidth,
-                        option,
-                        widget
-                        );
-
-                arrowOption.rect =
-                    QRect(
-                        0,
-                        0,
-                        indicatorSize,
-                        indicatorSize
-                        );
-                arrowOption.rect.moveCenter(
-                    option->rect.center()
-                    );
-
-                const PrimitiveElement arrow =
-                    option->state & QStyle::State_Open
-                        ? PE_IndicatorArrowDown
-                        : option->direction == Qt::RightToLeft
-                            ? PE_IndicatorArrowLeft
-                            : PE_IndicatorArrowRight;
-
-                QProxyStyle::drawPrimitive(
-                    arrow,
-                    &arrowOption,
+                drawBranchArrow(
+                    option,
                     painter,
                     widget
                     );
@@ -87,6 +63,162 @@ public:
             painter,
             widget
             );
+    }
+
+private:
+    void drawBranchArrow(
+        const QStyleOption* option,
+        QPainter* painter,
+        const QWidget* widget
+        ) const
+    {
+        if (!painter)
+        {
+            return;
+        }
+
+        const int indicatorSize =
+            QProxyStyle::pixelMetric(
+                QStyle::PM_IndicatorWidth,
+                option,
+                widget
+                );
+
+        QRectF indicatorRect(
+            0.0,
+            0.0,
+            indicatorSize + 2.0,
+            indicatorSize + 2.0
+            );
+        indicatorRect.moveCenter(
+            option->rect.center()
+            );
+
+        const bool enabled =
+            option->state & QStyle::State_Enabled;
+        const bool selected =
+            option->state & QStyle::State_Selected;
+        const bool hovered =
+            option->state & QStyle::State_MouseOver;
+
+        const QPalette::ColorGroup colorGroup =
+            enabled
+                ? (option->state & QStyle::State_Active
+                       ? QPalette::Active
+                       : QPalette::Inactive)
+                : QPalette::Disabled;
+
+        QColor arrowColor =
+            option->palette.color(
+                colorGroup,
+                selected
+                    ? QPalette::HighlightedText
+                    : hovered
+                        ? QPalette::Link
+                        : QPalette::Text
+                );
+
+        if (!selected && !hovered)
+        {
+            arrowColor.setAlpha(180);
+        }
+
+        painter->save();
+        painter->setRenderHint(
+            QPainter::Antialiasing,
+            true
+            );
+
+        if (hovered)
+        {
+            QColor backgroundColor =
+                option->palette.color(
+                    colorGroup,
+                    QPalette::Highlight
+                    );
+            backgroundColor.setAlpha(42);
+
+            painter->setPen(Qt::NoPen);
+            painter->setBrush(backgroundColor);
+            painter->drawRoundedRect(
+                indicatorRect.adjusted(
+                    1.0,
+                    1.0,
+                    -1.0,
+                    -1.0
+                    ),
+                4.0,
+                4.0
+                );
+        }
+
+        const QPointF center =
+            indicatorRect.center();
+        const qreal halfWidth = 3.5;
+        const qreal halfHeight = 3.5;
+
+        QPainterPath arrowPath;
+
+        if (option->state & QStyle::State_Open)
+        {
+            arrowPath.moveTo(
+                center.x() - halfWidth,
+                center.y() - 1.5
+                );
+            arrowPath.lineTo(
+                center.x(),
+                center.y() + 2.0
+                );
+            arrowPath.lineTo(
+                center.x() + halfWidth,
+                center.y() - 1.5
+                );
+        }
+        else if (option->direction == Qt::RightToLeft)
+        {
+            arrowPath.moveTo(
+                center.x() + 2.0,
+                center.y() - halfHeight
+                );
+            arrowPath.lineTo(
+                center.x() - 1.5,
+                center.y()
+                );
+            arrowPath.lineTo(
+                center.x() + 2.0,
+                center.y() + halfHeight
+                );
+        }
+        else
+        {
+            arrowPath.moveTo(
+                center.x() - 1.5,
+                center.y() - halfHeight
+                );
+            arrowPath.lineTo(
+                center.x() + 2.0,
+                center.y()
+                );
+            arrowPath.lineTo(
+                center.x() - 1.5,
+                center.y() + halfHeight
+                );
+        }
+
+        painter->setBrush(Qt::NoBrush);
+        painter->setPen(
+            QPen(
+                arrowColor,
+                1.8,
+                Qt::SolidLine,
+                Qt::RoundCap,
+                Qt::RoundJoin
+                )
+            );
+        painter->drawPath(
+            arrowPath
+            );
+        painter->restore();
     }
 };
 
